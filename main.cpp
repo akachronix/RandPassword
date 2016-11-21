6f6fe125bd3abeef694941e5ca8553ee378c8ed9
Brendan Gowen <bgowengaming1@gmail.com>
2016-11-21 12:22:34 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main(void)
+{
+  cout << "Password Generator v0.93" << endl;
+  cout << "by Brendan Gowen" << endl;
+  cout << "https://www.github.com/akachronix" << endl << endl;
+  
+  srand((unsigned)time(NULL));
+  
+  int userLength;
+  
+  cout << "How long to you want your password to be?" << endl;
+  cin >> userLength;
+  
+  cout << endl;
+  
+  string password;
+  
+  string characters[72]{ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" };
+  
+  int numberOfChars = sizeof(characters) / sizeof(characters[0]);
+  
+  cout << "Your password is: " << endl;
+  
+  for(int i = 0; i < userLength; i++)
+  {
+    int random = rand() % numberOfChars + 1;
+    cout<< characters[random];
+  }
+  
+  cout << password << endl;
+  
+  return 0;
+}
